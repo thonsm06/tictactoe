3f86ae28403a7979f4c96bd722173ced5b476676
Smith Thongbai <116685524+thonsm06@users.noreply.github.com>
2025-04-01 09:51:51 -0500

    Trying to fix line checking

--- script.js
+++ script.js
@@ -27,38 +27,53 @@ gameLoop = (function () {
 function getComputerMove() {
     let roll = 0;
     let val = -1;
+    const count = 0;
     do {
         roll = Math.floor(Math.random() * 9);
         val = gameBoard.board[roll];
+        if (count === 10) break;
     } while (val !== 0);
-    gameBoard.board[roll] = 1;
+    if (gameBoard.board.includes(0) === false) {return -1};
+    gameBoard.board[roll] = 2;
     grids[roll].textContent = "O";//mark box with user selection
-    console.log(`Success! Computer selection is grid ${roll}`);
     return roll;
 }
-
+const row1 = [0, 1, 2];
+const row2 = [3, 4, 5];
+const row3 = [6, 7, 8];
+const col1 = [0, 3, 6];
+const col2 = [1, 4, 7];
+const col3 = [2, 5, 8];
+const tl = [0, 4, 8];
+const tr = [2, 4, 6];
 function checkMove(lastUser) { //run this after making a move
     const board = gameBoard.board;
-    if (board[0] === board[1] && board[0] === board[2] || 
-        board[3] === board[4] && board[3] === board[5] || 
-        board[6] === board[7] && board[6] === board[8] || 
-        board[0] === board[3] && board[0] === board[6] ||
-        board[1] === board[4] && board[1] === board[7] ||
-        board[2] === board[5] && board[2] === board[8] || 
-        board[0] === board[4] && board[0] === board[8] ||
-        board[2] === board[4] && board[2] === board[6]) {
+    checkLine(board[0], board[1], board[2]);
+    if (board[0] === board[1] && board[0] === board[2] && board[0] !== 0 || 
+        board[3] === board[4] && board[3] === board[5] && board[3] !== 0 || 
+        board[6] === board[7] && board[6] === board[8] && board[6] !== 0 || 
+        board[0] === board[3] && board[0] === board[6] && board[0] !== 0 ||
+        board[1] === board[4] && board[1] === board[7] && board[1] !== 0 ||
+        board[2] === board[5] && board[2] === board[8] && board[2] !== 0 || 
+        board[0] === board[4] && board[0] === board[8] && board[0] !== 0 ||
+        board[2] === board[4] && board[2] === board[6] && board[2] !== 0) {
             //declare winner
             console.log(`Winner is ${lastUser}`);
             //run winner code here
 
-    }
+        }
+}
 
+function checkLine(line) {
+    const board = gameBoard.board;
+    if (board[0] !== 0 && board[0] === board[1] && board[0] === board[2]) {
+        console.log("winner");
+    }
 }
 function displayController(){
     //display board
 
 }
-
 
 const gridContainer = document.querySelector(".grid-container");
 gridContainer.addEventListener("click", (event => {
@@ -68,9 +83,12 @@ gridContainer.addEventListener("click", (event => {
         grid.textContent = "X";//mark box with user selection
         grid.setAttribute("value", "marked"); //avoid checking same box again
         gameBoard.board[grid.getAttribute("data-id")] = 1;
-        console.log(gameBoard);
+        checkLine(gameBoard.board[grid.getAttribute("data-id")]);
         checkMove("Player");
-        getComputerMove();
+        if (getComputerMove() === -1) {
+            //endgame
+        }
+        checkMove("Computer");
     }
 }));
 
@@ -83,5 +101,5 @@ function trackScore() {
 }
 
 function resetGame() {
-    
+
 }
